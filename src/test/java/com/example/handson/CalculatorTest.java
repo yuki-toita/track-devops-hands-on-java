06a84d73f622b6429b6b990c3125d9b4e5e29c08
yuki-toita <yuki.toita@tcs.com>
2026-06-12 13:48:59 +0900

    Update CalculatorTest.java

--- src/test/java/com/example/handson/CalculatorTest.java
+++ src/test/java/com/example/handson/CalculatorTest.java
@@ -1,15 +1,26 @@
 package com.example.handson;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
-@Test
-public void testAddIntegerValues() {
-Calculator calc = new Calculator();
-assertEquals(5.0, calc.add(2.0, 3.0));
+  @Test
+  public void testAddIntegerValues() {
+    Calculator calc = new Calculator();
+    
+    assertEquals(5.0, calc.add(2.0, 3.0));
 }
-@Test
-public void testAddDecimalValues() {
-Calculator calc = new Calculator();
-assertEquals(4.7, calc.add(1.2, 3.5), 0.0001);
-}
+  
+  @Test
+  public void testAddDecimalValues() {
+  Calculator calc = new Calculator();
+  
+    assertEquals(4.7, calc.add(1.2, 3.5), 0.0001);
+  }
+
+  @Test 
+  public void testAddThreeValues() {
+    Calculator calc = new Calculator();
+
+    assertEquals(6.0, calc.add(1.0, 2.0, 3.0));
+  }
 }
