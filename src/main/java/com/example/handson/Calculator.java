a4d3e155710eb8a0e88d403fa61cf1c64d46b162
yuki-toita <yuki.toita@tcs.com>
2026-06-12 13:44:47 +0900

    Update Calculator.java

--- src/main/java/com/example/handson/Calculator.java
+++ src/main/java/com/example/handson/Calculator.java
@@ -1,6 +1,9 @@
 package com.example.handson;
 public class Calculator {
-public double add(double a, double b) {
-return a + b;
-}
+  public double add(double a, double b) {
+    return a + b;
+  }
+  public double add(double a, double b, double c) {
+    return a + b + c;
+  }
 }
